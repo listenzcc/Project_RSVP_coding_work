9cb3cd3146d6bd28e2cc873e747f1b4ee1aa4622
zcc <listenzcc@mail.bnu.edu.cn>
2019-07-25 09:17:46 +0800

    modify Step-01-Cal...ICA...com, change _ica.fif to -ica.fif

--- Step-03-Save_epochs.py
+++ Step-03-Save_epochs.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
-# coding: utf-8
-
+import json
 import os
 import mne
 import time
@@ -11,14 +9,15 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 ''' ==============================================================
 Function : Setting independence parameters
-Outputs  : event_id, freq_l, freq_h, fir_design, ica
+Outputs  : event_id, freq_l, freq_h, fir_design, sfreq
 -------------------------------------------------------------- '''
 print('#:', time.ctime(), 'Setting independence parameters starts.')
 
 event_id = {'odd': 1, 'norm': 2, 'button': 3}
 freq_l, freq_h = 1, 50
 fir_design = 'firwin'
-ica = mne.preprocessing.ICA(n_components=0.95, method='fastica')
+tmin, tmax = -0.2, 1.0
+sfreq = 200
 root = 'd:\\RSVP_MEG_experiment'
 
 print('#:', time.ctime(), 'Setting independence parameters done.')
@@ -31,9 +30,8 @@ Outputs  : read_save_stuff
 print('#:', time.ctime(), 'Generating read_save_stuff starts.')
 
 newdir = os.path.join(root, 'epochs_saver', 'epochs_freq_1.0_50.0')
-# Protect existing results in newdir
-assert(not os.path.exists(newdir))
-os.mkdir(newdir)
+# Assume newdir exists
+assert(os.path.exists(newdir))
 
 read_save_stuff = {}
 
@@ -68,6 +66,15 @@ read_save_stuff['S02_meg'] = dict(
 print('#:', time.ctime(), 'Generating read_save_stuff done.')
 
 
+###############################
+# Reading bad_ICA_components table
+with open(os.path.join(newdir, 'bad_ICA_components.json'), 'r') as f:
+  x = f.readlines()
+  bad_ICA_components = json.loads(x[0])
+
+print(bad_ICA_components)
+
+
 ''' ==============================================================
 Function : For each stuff
 -------------------------------------------------------------- '''
@@ -79,10 +86,7 @@ for stuff in read_save_stuff.values():
 
         print(rawfile_path)
         print(report_dir)
-        if not os.path.exists(report_dir):
-            os.mkdir(report_dir)
-
-        figs = []
+        assert(os.path.exists(report_dir))
 
         ''' ==============================================================
         Function : Loading rawdata
@@ -95,6 +99,7 @@ for stuff in read_save_stuff.values():
             raw = mne.io.read_raw_ctf(rawfile_path)
             picks = mne.pick_types(raw.info, meg=True, ref_meg=False)
             events = mne.find_events(raw, stim_channel='UPPT001')
+            reject = dict(mag=4e-12)
         else:
             # if is EEG data
             raw = mne.io.read_raw_cnt(rawfile_path,
@@ -103,48 +108,34 @@ for stuff in read_save_stuff.values():
                                       stim_channel='STI 014')
             picks = mne.pick_types(raw.info, eeg=True)
             events = mne.find_events(raw)
+            reject = dict()
 
         raw.load_data()
         raw.filter(freq_l, freq_h, fir_design=fir_design)
-        figs.append(raw.plot_psd(tmax=np.inf, fmax=freq_h, show=False))
 
         print('#:', time.ctime(), 'Loading rawdata done.')
 
         ''' ==============================================================
-        Function : Calculating ICA
+        Function : Read and apply ICA
         Outputs  : ica: fitted ica instance
         -------------------------------------------------------------- '''
-        print('#:', time.ctime(), 'Calculating ICA starts.')
+        print('#:', time.ctime(), 'Reading ICA starts.')
 
-        ica.fit(raw, picks=picks)
-        [figs.append(f) for f in ica.plot_components(show=False)]
-        [figs.append(f) for f in ica.plot_properties(
-            raw, range(ica.n_components_), show=False)]
+        ica = mne.preprocessing.read_ica(os.path.join(report_dir, 'raw_ica.fif'))
+        ica.apply(raw, exclude=bad_ICA_components[os.path.basename(report_dir)])
 
-        print('#:', time.ctime(), 'Calculating ICA done.')
+        print('#:', time.ctime(), 'Reading ICA done.')
 
-        ''' ==============================================================
-        Function : Saving results
-        Outputs  : None
-        -------------------------------------------------------------- '''
-        print('#:', time.ctime(), 'Saving results starts.')
 
-        ica.save(os.path.join(report_dir, 'raw-ica.fif'))
+        ###############################
+        # Computing epochs
+        epochs = mne.Epochs(raw, picks=picks, events=events, event_id=event_id,
+                            tmin=tmin, tmax=tmax, baseline=None, detrend=1,
+                            reject=reject, preload=True)
+        epochs.resample(sfreq)
 
-        info = {e[0]: e[1] for e in raw.info.items()}
-        with open(os.path.join(report_dir, 'raw_info.txt'), 'w') as f:
-            for e in info.items():
-                f.write(e[0])
-                f.write(': ')
-                if e[1] is not None:
-                    f.write(str(e[1]))
-                f.write('\n')
+        ###############################
+        # Saving epochs
+        epochs.save(os.path.join(report_dir, 'sfreq200_cropn0.2p1.0-epo.fif'))
+        print(os.path.basename(report_dir), 'Saving epochs done.')
 
-        with PdfPages(os.path.join(
-                report_dir, 'raw_ica_report.pdf')) as pp:
-            for f in figs:
-                pp.savefig(f)
-
-        print('#:', time.ctime(), 'Saving results done.')
-
-        plt.close('all')
