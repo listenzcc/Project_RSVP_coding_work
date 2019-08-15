0159a2165dbd91439c578fd8b53350647e2d5e47
zcc <listenzcc@mail.bnu.edu.cn>
2019-08-15 09:05:17 +0800

    Change Step 03 Save_epochs to use mk_stuff_table

--- Step-03-Save_epochs.py
+++ Step-03-Save_epochs.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
 import json
 import os
 import mne
@@ -5,102 +8,55 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+from mk_stuff_table import stuff_table, epochs_root
 
 
-''' ==============================================================
-Function : Setting independence parameters
-Outputs  : event_id, freq_l, freq_h, fir_design, sfreq
--------------------------------------------------------------- '''
-print('#:', time.ctime(), 'Setting independence parameters starts.')
-
-event_id = {'odd': 1, 'norm': 2, 'button': 3}
+###############################
+# Prepare parameters
+event_id = {'odd': 1, 'norm': 2, 'button': 3, 'clear_norm': 4}
 freq_l, freq_h = 1, 50
 fir_design = 'firwin'
 tmin, tmax = -0.2, 1.0
 sfreq = 200
-root = 'd:\\RSVP_MEG_experiment'
-
-print('#:', time.ctime(), 'Setting independence parameters done.')
-
-
-''' ==============================================================
-Function : Generating read_save_stuff
-Outputs  : read_save_stuff
--------------------------------------------------------------- '''
-print('#:', time.ctime(), 'Generating read_save_stuff starts.')
-
-newdir = os.path.join(root, 'epochs_saver', 'epochs_freq_1.0_50.0')
-# Assume newdir exists
-assert(os.path.exists(newdir))
-
-read_save_stuff = {}
-
-read_save_stuff['S01_eeg'] = dict(
-    session_range=range(1, 11),
-    rawfile_path=os.path.join(root, 'rawdata',
-                              '20190402_RSVP_EEG_zhangchuncheng',
-                              'zcc%d.cnt'),
-    report_dir=os.path.join(newdir, 'EEG_S01_R%02d'))
-
-read_save_stuff['S02_eeg'] = dict(
-    session_range=range(1, 11),
-    rawfile_path=os.path.join(root, 'rawdata',
-                              '20190402_RSVP_EEG_maxuelin',
-                              'mxl%d.cnt'),
-    report_dir=os.path.join(newdir, 'EEG_S02_R%02d'))
-
-read_save_stuff['S01_meg'] = dict(
-    session_range=range(4, 11),
-    rawfile_path=os.path.join(root, 'rawdata',
-                              '20190326_RSVP_MEG_zhangchuncheng',
-                              'S01_lixiangTHU_20190326_%02d.ds'),
-    report_dir=os.path.join(newdir, 'MEG_S01_R%02d'))
-
-read_save_stuff['S02_meg'] = dict(
-    session_range=range(4, 12),
-    rawfile_path=os.path.join(root, 'rawdata',
-                              '20190326_RSVP_MEG_maxuelin',
-                              'S02_lixiangTHU_20190326_%02d.ds'),
-    report_dir=os.path.join(newdir, 'MEG_S02_R%02d'))
-
-print('#:', time.ctime(), 'Generating read_save_stuff done.')
-
-
-###############################
 # Reading bad_ICA_components table
-with open(os.path.join(newdir, 'bad_ICA_components.json'), 'r') as f:
-  x = f.readlines()
-  bad_ICA_components = json.loads(x[0])
+with open(os.path.join(epochs_root, 'bad_ICA_components.json'), 'r') as f:
+    x = f.readlines()
+    bad_ICA_components = json.loads(x[0])
 
 print(bad_ICA_components)
 
+###############################
+# Select subject 02 and meg dataset
+# stuff = stuff_table['MEG_S02']
+for stuff in stuff_table.values():
+    # User may check the stuff
+    for e in stuff.items():
+        print(e[0], ':', e[1])
 
-''' ==============================================================
-Function : For each stuff
--------------------------------------------------------------- '''
-for stuff in read_save_stuff.values():
-
+    ###############################
+    # For each session
     for session_id in stuff['session_range']:
         rawfile_path = stuff['rawfile_path'] % session_id
-        report_dir = stuff['report_dir'] % session_id
-
+        raw_ica_path = stuff['raw_ica_fif_path'] % session_id
+        epochs_path = stuff['epochs_path'] % session_id
+        print('-' * 60)
+        if os.path.exists(epochs_path):
+            print(epochs_path, 'exists, pass.')
+            continue
         print(rawfile_path)
-        print(report_dir)
-        assert(os.path.exists(report_dir))
+        print(raw_ica_path)
+        print(epochs_path)
 
-        ''' ==============================================================
-        Function : Loading rawdata
-        Outputs  : None
-        -------------------------------------------------------------- '''
-        print('#:', time.ctime(), 'Loading rawdata starts.')
-
+        # Read rawfile
+        is_legal_data = False
         if rawfile_path.endswith('.ds'):
             # if is MEG data
             raw = mne.io.read_raw_ctf(rawfile_path)
             picks = mne.pick_types(raw.info, meg=True, ref_meg=False)
             events = mne.find_events(raw, stim_channel='UPPT001')
             reject = dict(mag=4e-12)
-        else:
+            is_legal_data = True
+        if rawfile_path.endswith('.cnt'):
             # if is EEG data
             raw = mne.io.read_raw_cnt(rawfile_path,
                                       mne.channels.read_montage(
@@ -109,33 +65,44 @@ for stuff in read_save_stuff.values():
             picks = mne.pick_types(raw.info, eeg=True)
             events = mne.find_events(raw)
             reject = dict()
+            is_legal_data = True
+        assert(is_legal_data)
 
+        # Load rawfile and filter
         raw.load_data()
-        raw.filter(freq_l, freq_h, fir_design=fir_design)
+        raw.filter(freq_l, freq_h, fir_design=fir_design, verbose=1)
 
-        print('#:', time.ctime(), 'Loading rawdata done.')
+        # Apply ICA
+        ica = mne.preprocessing.read_ica(raw_ica_path)
+        ica.apply(raw, exclude=bad_ICA_components[
+            os.path.basename(os.path.dirname(raw_ica_path))])
 
-        ''' ==============================================================
-        Function : Read and apply ICA
-        Outputs  : ica: fitted ica instance
-        -------------------------------------------------------------- '''
-        print('#:', time.ctime(), 'Reading ICA starts.')
+        # Re-mark norm events as clear_norm if it not in +-1s range of odd
+        # Records ts: time points
+        ts = dict()
+        for id in event_id.keys():
+            print(id)
+            ts[id] = [e[0] for e in events if e[2] == event_id[id]]
 
-        ica = mne.preprocessing.read_ica(os.path.join(report_dir, 'raw_ica.fif'))
-        ica.apply(raw, exclude=bad_ICA_components[os.path.basename(report_dir)])
+        # Re-mark
+        new_events = events.copy()
+        for t in ts['norm']:
+            # For every norm events
+            if any(abs(t-x) < 1200 for x in ts['odd']):
+                # If in +-1s of odd, do not re-mark
+                continue
+            # Re-mark as 4, since it is clear
+            new_events[events[:, 0] == t, 2] = 4
 
-        print('#:', time.ctime(), 'Reading ICA done.')
-
-
-        ###############################
         # Computing epochs
-        epochs = mne.Epochs(raw, picks=picks, events=events, event_id=event_id,
+        epochs = mne.Epochs(raw, picks=picks,
+                            events=new_events, event_id=event_id,
                             tmin=tmin, tmax=tmax, baseline=None, detrend=1,
                             reject=reject, preload=True)
-        epochs.resample(sfreq)
+        epochs.resample(sfreq, verbose=2)
 
         ###############################
         # Saving epochs
-        epochs.save(os.path.join(report_dir, 'sfreq200_cropn0.2p1.0-epo.fif'))
-        print(os.path.basename(report_dir), 'Saving epochs done.')
-
+        print('Saving epochs.')
+        epochs.save(epochs_path, overwrite=False, verbose=2)
+        print('Done.')
