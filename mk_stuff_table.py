e14394c70017773fbb8cb083034a41c41e4c84ed
zcc <listenzcc@mail.bnu.edu.cn>
2019-08-15 09:04:37 +0800

    Rewrite mk_stuff_table to make it more readable

--- mk_stuff_table.py
+++ mk_stuff_table.py
@@ -6,109 +6,93 @@ import os
 ###############################
 # Make stuff table for access MEG data
 
-raw_data_root = 'd:\\RSVP_MEG_experiment\\rawdata'
-epochs_root = 'd:\\RSVP_MEG_experiment\\epochs_saver'
+raw_data_root = os.path.join('d:\\', 'RSVP_MEG_experiment', 'rawdata')
+epochs_root = os.path.join(
+    'd:\\', 'RSVP_MEG_experiment', 'epochs_saver', 'epochs_freq_1.0_50.0')
 freesurfer_root = 'd:\\freesurfer\\subjects'
 
 epochs_freq_ = 'epochs_freq_1.0_50.0'
 
+event_id = {'odd': 1, 'norm': 2, 'button': 3, 'clear_norm': 4}
+
 stuff_table = dict()
 
-###############################
-# Subject 01
-MEG_S01_R_ = 'MEG_S01_R%02d'
-stuff_table['S01_meg'] = dict(
-    subject_truename='zhangchuncheng',
-    subject_id='S01',
-    subject_freesurfer='s01_for_mne',
-    session_range=range(4, 11),
-    rawfile_path=os.path.join(
-        raw_data_root,
-        '20190326_RSVP_MEG_zhangchuncheng',
-        'S01_lixiangTHU_20190326_%02d.ds'
-        ),
-    epochs_path=os.path.join(
-        epochs_root,
-        epochs_freq_,
-        MEG_S01_R_,
-        'sfreq200_cropn0.2p1.0-epo.fif'
-        ),
-    denoised_epochs_path=os.path.join(
-        epochs_root,
-        epochs_freq_,
-        MEG_S01_R_,
-        'sfreq200_cropn0.2p1.0-den_epo.fif'
-        ),
-    raw_ica_report_path=os.path.join(
-        epochs_root,
-        epochs_freq_,
-        MEG_S01_R_,
-        'raw_ica_report.pdf',
-        ),
-    cov_path=os.path.join(
-        epochs_root,
-        epochs_freq_,
-        MEG_S01_R_,
-        'xxx-epo-cov.fif'),
-    epochs_ts_report_path=os.path.join(
-        epochs_root,
-        epochs_freq_,
-        MEG_S01_R_,
-        'epochs_ts_report.pdf',
-        ),
-    trans=os.path.join(
-        freesurfer_root,
-        's01_for_mne/s01_coreg-trans.fif'
-        ),
-)
+for subject_id, rawfile_id, session_range in [
+        # MEG Subject 01
+        ['MEG_S01',
+         os.path.join('20190326_RSVP_MEG_zhangchuncheng',
+                      'S01_lixiangTHU_20190326_%02d.ds'),
+         range(4, 11)],
+        # MEG Subject 02
+        ['MEG_S02',
+         os.path.join('20190326_RSVP_MEG_maxuelin',
+                      'S02_lixiangTHU_20190326_%02d.ds'),
+         range(4, 12)],
+        # EEG Subject 01
+        ['EEG_S01',
+        os.path.join('20190402_RSVP_EEG_zhangchuncheng',
+                              'zcc%d.cnt'),
+        range(1, 11)],
+        # EEG Subject 02
+        ['EEG_S02',
+        os.path.join('20190402_RSVP_EEG_maxuelin',
+                              'mxl%d.cnt'),
+        range(1, 11)],
+]:
 
-###############################
-# Subject 02
-MEG_S02_R_ = 'MEG_S02_R%02d'
-stuff_table['S02_meg'] = dict(
-    subject_truename='maxuelin',
-    subject_id='S02',
-    subject_freesurfer='s02_for_mne',
-    session_range=range(4, 11),
-    rawfile_path=os.path.join(
-        raw_data_root,
-        '20190326_RSVP_MEG_maxuelin',
-        'S02_lixiangTHU_20190326_%02d.ds'
-        ),
-    epochs_path=os.path.join(
-        epochs_root,
-        epochs_freq_,
-        MEG_S02_R_,
-        'sfreq200_cropn0.2p1.0-epo.fif'
-        ),
-    denoised_epochs_path=os.path.join(
-        epochs_root,
-        epochs_freq_,
-        MEG_S02_R_,
-        'sfreq200_cropn0.2p1.0-den_epo.fif'
-        ),
-    raw_ica_report_path=os.path.join(
-        epochs_root,
-        epochs_freq_,
-        MEG_S02_R_,
-        'raw_ica_report.pdf',
-        ),
-    cov_path=os.path.join(
-        epochs_root,
-        epochs_freq_,
-        MEG_S02_R_,
-        'xxx-epo-cov.fif'
-        ),
-    epochs_ts_report_path=os.path.join(
-        epochs_root,
-        epochs_freq_,
-        MEG_S02_R_,
-        'epochs_ts_report.pdf',
-        ),
-    trans=os.path.join(
+    working_path = os.path.join(epochs_root, '%s_R%s' %
+                                (subject_id, '%02d'))
+
+    print('\n'.join(['='*80, subject_id, rawfile_id, working_path]))
+
+    stuff_table[subject_id] = {
+        # Subject id
+        'subject_id': subject_id,
+
+        # Main working path
+        'working_path': working_path,
+
+        # The sessions we used
+        'session_range': session_range,
+
+        # Original raw file
+        'rawfile_path': os.path.join(
+            raw_data_root,
+            rawfile_id),
+
+        # ICA fif file on raw fif
+        'raw_ica_fif_path': os.path.join(
+            working_path, 'raw_ica.fif'),
+
+        # ICA report
+        'raw_ica_report_path': os.path.join(
+            working_path, 'raw_ica_report.pdf'),
+
+        # Epochs from Filtered and ICAed raw file
+        'epochs_path': os.path.join(
+            working_path, 'raw-epo.fif'),
+
+        # Evoked joints of raw-epo
+        'evoked_joint_report_path': os.path.join(
+            working_path, 'evoked_joint_report.pdf'),
+
+        # Covariance fif on raw-epo
+        'cov_path': os.path.join(
+            working_path, 'raw-epo-cov.fif'),
+
+        # Denoised epochs using Xdawn
+        'denoised_epochs_path': os.path.join(
+            working_path, 'denoised-epo.fif'),
+
+        # Todo
+        # Freesurfer path
+        # Forward solution path
+    }
+
+
+'''
+trans=os.path.join(
         freesurfer_root,
         's02_for_mne/s02_coreg-trans.fif'
-        ),
-)
-
-print(stuff_table)
+    ),
+'''
