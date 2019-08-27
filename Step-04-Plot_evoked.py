45591444517fe9c123c700de093f13ba3d396eb1
zcc <listenzcc@mail.bnu.edu.cn>
2019-08-27 15:07:34 +0800

    Unstage Step-04 and commit new Step-06, plot evoked is necessary, different bands analysis is on next step.

--- Step-04-Plot_evoked.py
+++ Step-04-Plot_evoked.py
@@ -1,19 +1,11 @@
 # coding: utf-8
 
-import os
 import mne
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from mk_stuff_table import stuff_table
 
-######################
-# Setting parameters
-# Events
 event_id = {'odd': 1, 'norm': 2, 'button': 3, 'clear_norm': 4}
-# Freq parameters
-freqs_list = [(1, 4), (4, 7), (7, 12), (7, 9), (9, 12)]
-# n_jobs
-n_jobs = 1
 
 # stuff = stuff_table['MEG_S02']
 for stuff in stuff_table.values():
@@ -24,55 +16,45 @@ for stuff in stuff_table.values():
         print(session_id)
 
         epochs_path = stuff['epochs_path'] % session_id
-        _report_path = stuff['evoked_joint_report_path'] % session_id
+        report_path = stuff['evoked_joint_report_path'] % session_id
+        epochs = mne.read_epochs(epochs_path)
+        print(epochs)
 
-        dir_name = os.path.dirname(_report_path)
-        base_name = os.path.basename(_report_path)
+        figs = []
+        for e in epochs.event_id.keys():
+            epo = epochs[e]
+            print(epo)
+            times = epo.times[::20]
 
-        for freqs in freqs_list:
-            l_freq, h_freq = freqs
-            report_path = os.path.join(dir_name, 'band_%d_%d_' % (l_freq, h_freq)+base_name)
+            # fig = epo.plot_psd_topomap(n_jobs=8, verbose=2, show=False)
+            # fig.suptitle(e)
+            # figs.append(fig)
 
-            epochs = mne.read_epochs(epochs_path)
-            epochs = epochs.filter(l_freq=l_freq, h_freq=h_freq,
-                                   n_jobs=n_jobs, verbose=2)
-            print(epochs)
+            evoked = epo.average()
+            # fig = evoked.plot(gfp=True, spatial_colors=True, show=False)
+            # fig.suptitle(e)
+            # figs.append(fig)
 
-            figs = []
-            for e in epochs.event_id.keys():
-                epo = epochs[e]
-                print(epo)
-                times = epo.times[::20]
+            # fig = evoked.plot_topomap(times=times, time_unit='s', show=False)
+            # fig.suptitle(e)
+            # figs.append(fig)
 
-                # fig = epo.plot_psd_topomap(n_jobs=8, verbose=2, show=False)
-                # fig.suptitle(e)
-                # figs.append(fig)
+            ts_args = dict(gfp=True, time_unit='s')
+            topomap_args = dict(sensors=False, time_unit='s')
+            fig = evoked.plot_joint(title=e, times=times,
+                                    ts_args=ts_args,
+                                    topomap_args=topomap_args,
+                                    show=False)
+            figs.append(fig)
 
-                evoked = epo.average()
-                # fig = evoked.plot(gfp=True, spatial_colors=True, show=False)
-                # fig.suptitle(e)
-                # figs.append(fig)
+            fig = evoked.plot_joint(title=e, times='peaks',
+                                    ts_args=ts_args,
+                                    topomap_args=topomap_args,
+                                    show=False)
+            figs.append(fig)
 
-                # fig = evoked.plot_topomap(times=times, time_unit='s', show=False)
-                # fig.suptitle(e)
-                # figs.append(fig)
+        with PdfPages(report_path, 'w') as pp:
+            for f in figs:
+                pp.savefig(f)
 
-                ts_args = dict(gfp=True, time_unit='s')
-                topomap_args = dict(sensors=False, time_unit='s')
-                fig = evoked.plot_joint(title=e, times=times,
-                                        ts_args=ts_args,
-                                        topomap_args=topomap_args,
-                                        show=False)
-                figs.append(fig)
-
-                fig = evoked.plot_joint(title=e, times='peaks',
-                                        ts_args=ts_args,
-                                        topomap_args=topomap_args,
-                                        show=False)
-                figs.append(fig)
-
-            with PdfPages(report_path, 'w') as pp:
-                for f in figs:
-                    pp.savefig(f)
-
-            plt.close('all')
+        plt.close('all')
