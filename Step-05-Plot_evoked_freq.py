ce6bf724d4465716542018671a2308eaf46e396a
zcc <listenzcc@mail.bnu.edu.cn>
2019-09-02 14:27:12 +0800

    add multi frequency bands for Plot_evoked_freq

--- Step-05-Plot_evoked_freq.py
+++ Step-05-Plot_evoked_freq.py
@@ -11,9 +11,16 @@ from mk_stuff_table import stuff_table
 # Setup parameters
 event_id = {'odd': 1, 'norm': 2, 'button': 3, 'clear_norm': 4}
 # define frequencies of interest (log-spaced)
-freqs = np.logspace(*np.log10([1, 50]), num=20)
+freqs = np.logspace(*np.log10([1, 50]), num=50)
+# freqs = np.linspace(1, 50, 50)
 # different number of cycle per frequency
 n_cycles = freqs / 2
+# bands
+bands = [(0, 4, 'Delta'),
+         (4, 8, 'Theta'),
+         (8, 12, 'Alpha'),
+         (12, 30, 'Beta'),
+         (30, 45, 'Gamma')]
 
 ######################
 # Read epochs and plot psd topomaps
@@ -37,21 +44,21 @@ for stuff in stuff_table.values():
         ######################
         # Plot psd topomaps and store them in figs, a list for figures
         figs = []
-        for e in epochs.event_id.keys():
+        for eid in epochs.event_id.keys():
             # Fetch epochs of certain event
-            epo = epochs[e]
+            epo = epochs[eid]
             print(epo)
 
             # Plot overall psd
             f = epo.plot_psd(fmax=50, show=False)
             f.set_figheight(2.5)
-            f.suptitle('%s %s' % (stuff['subject_id'], e))
+            f.suptitle('%s %s' % (stuff['subject_id'], eid))
             figs.append(f)
 
             # Plot psd topomaps for Delta, Theta, Alpha, Beta, Gamma band
             f = epo.plot_psd_topomap(normalize=True, show=False)
             f.set_figheight(2.5)
-            f.suptitle('%s %s' % (stuff['subject_id'], e))
+            f.suptitle('%s %s' % (stuff['subject_id'], eid))
             figs.append(f)
 
             # Calculate tfr_morlet power and itc
@@ -61,8 +68,15 @@ for stuff in stuff_table.values():
             # Plot wavelet power
             f = power.plot_joint(baseline=(-0.2, 0), mode='mean',
                                  show=False)
-            f.suptitle('%s %s' % (stuff['subject_id'], e))
+            f.suptitle('%s %s' % (stuff['subject_id'], eid))
             figs.append(f)
+            for band in bands:
+                f = power.plot_joint(baseline=(-0.2, 0), mode='mean',
+                                     fmin=band[0], fmax=band[1],
+                                     timefreqs=((0.3, (band[0]+band[1])/2)),
+                                     show=False)
+                f.suptitle('%s %s %s' % (stuff['subject_id'], eid, band[2]))
+                figs.append(f)
 
         # Print figures in PDF file
         with PdfPages(report_path, 'w') as pp:
