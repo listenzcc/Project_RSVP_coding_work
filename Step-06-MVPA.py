3996e27784847b3e2c23012738c7799c625ebfa9
zcc <listenzcc@mail.bnu.edu.cn>
2019-09-03 15:04:02 +0800

    Add Step-06-MVPA. It is a code from server. Remove accs_xxx since they are no longer needed.

--- Step-06-MVPA.py
+++ Step-06-MVPA.py
@@ -17,13 +17,17 @@ from sklearn.model_selection import LeaveOneGroupOut
 
 from mk_stuff_table import stuff_table
 
+bands = [(0, 4, 'Delta'),
+         (4, 8, 'Theta'),
+         (8, 12, 'Alpha'),
+         (12, 30, 'Beta'),
+         (30, 45, 'Gamma')]
 ######################
 # Setting parameters
 # Events
 event_id = {'odd': 1, 'norm': 2, 'button': 3, 'clear_norm': 4}
-# Freq parameters
-l_freq, h_freq = 1, 7  # Hz
-h_trans_bandwidth = 3  # Hz
+# Time range
+tmin, tmax = 0.0, 0.8
 # n_jobs
 n_jobs = 16
 
@@ -35,10 +39,11 @@ data_id = sys.argv[-1]
 # Else use 'MEG_S02' as fname
 if data_id not in stuff_table.keys():
     data_id = 'MEG_S02'
-
-tmp_path = 'tmp'
-pkl_path = os.path.join(tmp_path, data_id + '.pkl')
-print(pkl_path)
+pkl_folder = os.path.join(
+    '/nfs/cell_a/userhome/zcc/documents/RSVP_experiment/Project_RSVP_coding_work/MVPA_results')
+print(pkl_folder)
+if not os.path.exists(pkl_folder):
+    os.mkdir(pkl_folder)
 
 stuff = stuff_table[data_id]
 for e in stuff.items():
@@ -48,18 +53,16 @@ for e in stuff.items():
 # Read epochs from file
 epochs_list = []
 group_list = []
-for session_id in [5, 7]:  # stuff['session_range']:
+for session_id in stuff['session_range']:
     epochs_path = stuff['epochs_path'] % session_id
     print(epochs_path)
     # Read epochs
-    # Select one option for operation
-    # Option 1, fetch odd and clear_norm epochs
-    _epochs = mne.read_epochs(epochs_path)[['odd', 'clear_norm']]
-    # Option 2, fetch odd, norm and clear_norm epochs
-    # _epochs = mne.read_epochs(epochs_path)[['odd', 'norm', 'clear_norm']]
-    # _epochs.events[_epochs.events[:, -1] == 4, -1] = 2
 
-    _epochs = _epochs.crop(tmin=0.0, tmax=0.8)
+    _epochs = mne.read_epochs(epochs_path)[['odd', 'clear_norm']]
+    # _epochs = mne.read_epochs(epochs_path)[['odd', 'norm', 'clear_norm']]
+    # _epochs.events[_epochs.events[:, -1]==4, -1] = 2
+    # _epochs = _epochs[['odd', 'norm']]
+    _epochs = _epochs.crop(tmin=tmin, tmax=tmax)
 
     # !!! 'Align' epochs
     if len(epochs_list) > 0:
@@ -72,13 +75,8 @@ for session_id in [5, 7]:  # stuff['session_range']:
 
 ######################
 # Concatenate epochs in epochs_list
-epochs = mne.epochs.concatenate_epochs(epochs_list)
+epochs_concatenate = mne.epochs.concatenate_epochs(epochs_list)
 groups = np.concatenate(group_list).squeeze()
-# Filter epochs, filter function seems not applied in-place
-epochs = epochs.filter(l_freq=l_freq, h_freq=h_freq,
-                       h_trans_bandwidth=h_trans_bandwidth,
-                       n_jobs=n_jobs, verbose=2)
-# epochs = epochs.apply_baseline(baseline=(-0.2, 0))
 
 ######################
 # MVPA
@@ -95,89 +93,116 @@ svm = svm.SVC(gamma='scale', kernel='rbf', class_weight=class_weight)
 # Init decoder
 clf = make_pipeline(StandardScaler(), svm)
 # Raw 3-D data decoder
-raw_decoder = make_pipeline(mne.decoding.Vectorizer(), StandardScaler(), clf)
-# Time resolution data decoder
+raw_decoder = make_pipeline(mne.decoding.Vectorizer(), clf)
+# Time resolution decoder
 time_decoder = SlidingEstimator(clf, n_jobs=n_jobs, scoring='f1', verbose=1)
 
 # Init time information
-times = epochs.times
+times = epochs_concatenate.times
 n_times = len(times)
-y_true = epochs.events[:, 2]
+y_true = epochs_concatenate.events[:, 2]
 n_samples = len(y_true)
 # Time window information
-sfreq = epochs.info['sfreq']
-window_info = {}
+sfreq = epochs_concatenate.info['sfreq']
+time_windows = {}
 y_pred_timewindow = {}
-for window_length in [0.1, 0.2, 0.3, 0.4]:
+# window_length refers time length in seconds
+for window_length in [0.05, 0.1, 0.2, 0.3, 0.4]:
+    # Length in time points
     w_length = int(sfreq * window_length)
+    # Step in time points
     w_step = int(sfreq * window_length / 2)
+    # Beginnings in time points
     w_start = np.arange(0, n_times-w_length, w_step)
+    # The time stamps of the windows
     w_time = np.empty(len(w_start))
     for j, s in enumerate(w_start):
         w_time[j] = (times[s] + times[s+w_length]) / 2
+    # Setup label list for each window_length
+    # It is a matrix with size of n_samples x len(w_time)
     y_pred_timewindow[window_length] = np.empty([n_samples, len(w_time)])
-    window_info[window_length] = [w_start, w_length, w_time]
+    # Storage w_start, w_length, w_time
+    time_windows[window_length] = [w_start, w_length, w_time]
 
-# Predicted map
-map_pred = np.zeros(n_samples)
-# Prediction of all time decoding
-y_pred = np.empty(n_samples)
-# Prediction of time resolution decoding
-y_pred_time = np.empty([n_samples, n_times])
+######################
+# MVPA for each band
+for band in bands:
+    # Setup parameters
+    # stop freq
+    l_freq, h_freq = band[0], band[1]
+    # pkl_path, pkl file path for storage results
+    pkl_path = os.path.join(pkl_folder, '%s-%s.pkl' % (data_id, band[2]))
 
-for train_index, test_index in logo.split(y_true, y_true, groups):
-    # Cross-validation across groups, groups mean different sessions
-    print('=' * 80)
-    print(epochs[train_index])
-    print(epochs[test_index])
+    # Copy epochs
+    epochs = epochs_concatenate.copy()
 
-    # Xdawn denoise
-    # fit_transform
-    X_train = xdawn.fit_transform(epochs[train_index])
-    y_train = y_true[train_index]
-    # transform
-    X_test = xdawn.transform(epochs[test_index])
-    y_test = y_true[test_index]
+    # Filter epochs, filter function seems not applied in-place
+    epochs = epochs.filter(l_freq=l_freq, h_freq=h_freq,
+                           n_jobs=n_jobs, verbose=2)
 
-    # All time decoder
-    # Fit
-    raw_decoder.fit(X_train, y_train)
-    # Predict
-    y_pred[test_index] = raw_decoder.predict(X_test)
+    # Predicted map
+    map_pred = np.zeros(n_samples)
+    # Prediction of all time decoding
+    y_pred = np.empty(n_samples)
+    # Prediction of time resolution decoding
+    y_pred_time = np.empty([n_samples, n_times])
 
-    # Time decoder
-    # Fit
-    time_decoder.fit(X_train, y_train)
-    # Predict
-    y_pred_time[test_index] = time_decoder.predict(X_test)
+    for train_index, test_index in logo.split(y_true, y_true, groups):
+        # Cross-validation across groups, groups mean different sessions
+        print('=' * 80)
+        print(epochs[train_index])
+        print(epochs[test_index])
 
-    # Time window decoder
-    for window_length, info in window_info.items():
-        length = info[1]
-        for j, start in enumerate(info[0]):
-            stop = start + length
-            print(start)
-            # Crop X_train and X_test
-            _X_train = X_train[:, :, start:start+w_length]
-            _X_test = X_test[:, :, start:start+w_length]
-            # Fit
-            raw_decoder.fit(_X_train, y_train)
-            y_pred_timewindow[window_length][test_index,
-                                             j] = raw_decoder.predict(_X_test)
+        # Xdawn denoise
+        # fit_transform
+        X_train = xdawn.fit_transform(epochs[train_index])
+        y_train = y_true[train_index]
+        # transform
+        X_test = xdawn.transform(epochs[test_index])
+        y_test = y_true[test_index]
 
-    # Mark examples that are tested
-    map_pred[test_index] = 1
-    print('%d | %d samples solved.' % (sum(map_pred), n_samples))
-    print('-' * 80)
+        # All time decoder
+        # Fit
+        raw_decoder.fit(X_train, y_train)
+        # Predict
+        y_pred[test_index] = raw_decoder.predict(X_test)
 
-results = dict(
-    times=times,
-    window_info=window_info,
-    y_true=y_true,
-    y_pred=y_pred,
-    y_pred_time=y_pred_time,
-    y_pred_timewindow=y_pred_timewindow,
-)
+        # Time decoder
+        # Fit
+        time_decoder.fit(X_train, y_train)
+        # Predict
+        y_pred_time[test_index] = time_decoder.predict(X_test)
 
-with open(pkl_path, 'wb') as f:
-    pickle.dump(results, f)
+        # Time window decoder
+        for window_length, _info in time_windows.items():
+            # _info[0], w_start
+            # _info[1], w_length
+            # _info[2], w_time
+            w_length = _info[1]
+            for j, w_start in enumerate(_info[0]):
+                w_stop = w_start + w_length
+                print(w_start)
+                # Crop X_train and X_test
+                _X_train = X_train[:, :, w_start:w_start+w_length]
+                _X_test = X_test[:, :, w_start:w_start+w_length]
+                # Fit
+                raw_decoder.fit(_X_train, y_train)
+                y_pred_timewindow[window_length][
+                    test_index, j] = raw_decoder.predict(_X_test)
+
+        # Mark examples that have been tested
+        map_pred[test_index] = 1
+        print('%d | %d samples solved.' % (sum(map_pred), n_samples))
+        print('-' * 80)
+
+    results = dict(
+        times=times,
+        time_windows=time_windows,
+        y_true=y_true,
+        y_pred=y_pred,
+        y_pred_time=y_pred_time,
+        y_pred_timewindow=y_pred_timewindow,
+    )
+
+    with open(pkl_path, 'wb') as f:
+        pickle.dump(results, f)
